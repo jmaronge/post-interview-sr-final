516f06d9da502327d32cd56bb5eb88c397cc3199
jmaronge <jmaronge@ymail.com>
2020-04-20 08:30:13 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# post-interview-sr-final
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/post-interview-sr-final)
---
+++ src/app/post-list/post-list.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit, Input, EventEmitter, Output } from '@angular/core';
+
+export class Post {
+  id: number;
+  message: string;
+}
+
+@Component({
+  selector: 'post-list',
+  templateUrl: './post-list.component.html',
+  styleUrls: ['./post-list.component.css']
+})
+export class PostListComponent  {
+
+  @Input() posts: Array<Post> = [];
+  public selectedPost: Post;
+
+  @Output() postClick: EventEmitter<Post> = new EventEmitter<Post>();
+
+  constructor() { }
+
+   postClickLocal($event, post){
+     $event.stopPropagation();
+     $event.preventDefault();
+     this.selectedPost = post;
+     this.postClick.emit(post)
+   }
+
+}
